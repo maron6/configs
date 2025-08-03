cc708d9543c0060a67bbe6d8b9202d760b0d80d8
Ryan Langer <rlanger579@gmail.com>
2025-08-03 14:48:19 -0700

    Add lsd to config

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -107,6 +107,7 @@
     bitwarden-desktop
     github-desktop
     nixfmt-rfc-style
+    lsd
     # discord-ptb # move to gaming.nix?
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   ];
