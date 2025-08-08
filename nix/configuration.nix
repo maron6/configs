ba72251132f937baad810a352749e6e63bfac918
Ryan Langer <rlanger579@gmail.com>
2025-08-07 18:00:45 -0700

    Enable ssh daemon/agent

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -153,7 +153,8 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
+  programs.ssh.startAgent = true;
 
   #services.flatpak.enable = true;
   #xdg.portal.enable = true; # for flatpak
